19d6953f12ef0e8ba7dfb1b5da0e5e3bc18311e1
Bilal Waseem <bilalwc42@gmail.com>
2018-02-16 17:00:36 +0500

    2. Added interface + metamask support

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,25 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-    //member variables
-    string public candidate;
+    //model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates; //associative array named candidates like a hash map i.e. assigns a uint for each candidate where uint is from the struct object and candidate is a single object. The mapping function interacts with the datalayer of the block
+
+    uint public candidatesCount; //default value is zero
+
     //constructor
     function Election() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private {
+        candidatesCount++;                      //id, name, voteCount
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0); //stores the object in the mapping array
     }
 }
